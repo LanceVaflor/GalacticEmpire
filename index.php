604a771fca41f7b48f9e9735b73a188db073935f
Lance <60122560+LanceVaflor@users.noreply.github.com>
2020-09-05 13:53:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -61,5 +61,9 @@
         </div>
 
     </body>
-    <footer></footer>
+    <footer>
+       <a href="https://github.com/LanceVaflor/GalacticEmpire" id="GitHub"> GitHub </a>
+       <a href="www.facebook.com" id="Facebook"> Facebook </a>
+       <a href="www.twitter.com" id="Twitter"> Twitter </a>
+    </footer>
 </html>
